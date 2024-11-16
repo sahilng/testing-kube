81135c1d6c70f345c26846c8b82f651c3d75d125
Sahil Gupta <sahilng1997@gmail.com>
2024-11-16 14:10:52 -0500

    update

--- expose.sh
+++ expose.sh
@@ -3,4 +3,5 @@
 sudo apt install socat -y
 echo ""
 echo "Relaying port 80 on the current machine to the flask-service in the minikube cluster..."
-sudo socat TCP-LISTEN:80,fork TCP:192.168.49.2:30080
+echo "socat TCP-LISTEN:80,fork TCP:$(minikube ip):30080"
+sudo socat TCP-LISTEN:80,fork TCP:$(minikube ip):30080
